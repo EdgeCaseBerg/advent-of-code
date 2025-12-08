ebd350248ca5de430c6835072ea74caca7b2a1c7
peetseater <peetseater@gmail.com>
2025-12-08 07:07:05 -0500

    D8P1 swap point type for overflow

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -13,22 +13,27 @@ fn main() {
     println!("{:?}", i.elapsed());
 }
 
-type Tuple3 = (usize, usize, usize); 
+type PointType = i128;
+type Tuple3 = (PointType, PointType, PointType); 
 
 fn p1(raw_data: &str) {
     let points: Vec<Tuple3> = raw_data.lines().take_while(|line| !line.is_empty()).map(|line| {
         let mut iter = line.split(",");
         (
-            iter.next().expect("Option not defined x").parse::<usize>().expect("Could not parse number x"),
-            iter.next().expect("Option not defined y").parse::<usize>().expect("Could not parse number y"),
-            iter.next().expect("Option not defined z").parse::<usize>().expect("Could not parse number z")
+            iter.next().expect("Option not defined x").parse::<PointType>().expect("Could not parse number x"),
+            iter.next().expect("Option not defined y").parse::<PointType>().expect("Could not parse number y"),
+            iter.next().expect("Option not defined z").parse::<PointType>().expect("Could not parse number z")
         )
     }).collect();
     println!("{:?}", points);
-    println!("{:?}", euclidean_distance(points[0], points[1]));
+    let test_point = points[0];
+    for point in points.iter().skip(1) {
+        println!("{:?}", euclidean_distance(&test_point, point));
+    }
+    
 }
 
-fn euclidean_distance(p1:Tuple3, p2: Tuple3) -> usize {
+fn euclidean_distance(p1: &Tuple3, p2: &Tuple3) -> PointType {
     let n = (p1.0 - p2.0) * (p1.0 - p2.0) +
     (p1.1 - p2.1) * (p1.1 - p2.1) +
     (p1.2 - p2.2) * (p1.2 - p2.2);
