143132e84c6e93b72319ea22d6b5c426d043bb7b
peetseater <peetseater@gmail.com>
2025-12-08 07:03:47 -0500

    D8P1 euclid_distance

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -13,8 +13,10 @@ fn main() {
     println!("{:?}", i.elapsed());
 }
 
+type Tuple3 = (usize, usize, usize); 
+
 fn p1(raw_data: &str) {
-    let points: Vec<(usize, usize, usize)> = raw_data.lines().take_while(|line| !line.is_empty()).map(|line| {
+    let points: Vec<Tuple3> = raw_data.lines().take_while(|line| !line.is_empty()).map(|line| {
         let mut iter = line.split(",");
         (
             iter.next().expect("Option not defined x").parse::<usize>().expect("Could not parse number x"),
@@ -23,6 +25,14 @@ fn p1(raw_data: &str) {
         )
     }).collect();
     println!("{:?}", points);
+    println!("{:?}", euclidean_distance(points[0], points[1]));
+}
+
+fn euclidean_distance(p1:Tuple3, p2: Tuple3) -> usize {
+    let n = (p1.0 - p2.0) * (p1.0 - p2.0) +
+    (p1.1 - p2.1) * (p1.1 - p2.1) +
+    (p1.2 - p2.2) * (p1.2 - p2.2);
+    n.isqrt()
 }
 
 
