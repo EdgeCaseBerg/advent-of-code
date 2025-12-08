e20809f5b5a7fda90f95cce96fe8b2dfb6a38c18
peetseater <peetseater@gmail.com>
2025-12-08 06:55:07 -0500

    setup p1 and p2

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = fs::read_to_string("./input").expect("bad input data");
+    let raw_data = raw_data.as_str();
+    let i = Instant::now();
+    p1(raw_data);
+    println!("{:?}", i.elapsed());
+
+    let i = Instant::now();
+    p2(raw_data);
+    println!("{:?}", i.elapsed());
 }
+
+fn p1(_raw_data: &str) {
+}
+
+fn p2(_raw_data: &str) {}
