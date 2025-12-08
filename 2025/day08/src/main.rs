68559bbdce65d84d1d2bb56ae3b84ab23068335e
peetseater <peetseater@gmail.com>
2025-12-08 06:57:57 -0500

    D8P1 input parsed

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -13,7 +13,17 @@ fn main() {
     println!("{:?}", i.elapsed());
 }
 
-fn p1(_raw_data: &str) {
+fn p1(raw_data: &str) {
+    let points: Vec<(usize, usize, usize)> = raw_data.lines().take_while(|line| !line.is_empty()).map(|line| {
+        let mut iter = line.split(",");
+        (
+            iter.next().expect("Option not defined x").parse::<usize>().expect("Could not parse number x"),
+            iter.next().expect("Option not defined y").parse::<usize>().expect("Could not parse number y"),
+            iter.next().expect("Option not defined z").parse::<usize>().expect("Could not parse number z")
+        )
+    }).collect();
+    println!("{:?}", points);
 }
+
 
 fn p2(_raw_data: &str) {}
