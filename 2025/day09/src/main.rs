ee39e65ae17e319dd9956e5c8d03467f16524f6a
peetseater <peetseater@gmail.com>
2025-12-09 00:15:09 -0500

    D9P1 done

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -17,15 +17,29 @@ fn main() {
     println!("Took: {:?}", time);
 }
 
-fn p1(raw_data: &str) {
-    let tiles: Vec<(usize, usize)> = raw_data.lines().take_while(|line| !line.is_empty()).map(|line| {
+type ResultType = i128;
+
+fn p1(raw_data: &str) -> ResultType {
+    let tiles: Vec<(ResultType, ResultType)> = raw_data.lines().take_while(|line| !line.is_empty()).map(|line| {
         let mut xy = line.split(",");
         (
             xy.next().expect("no digit x").parse().expect("bad number x"),
             xy.next().expect("no digit y").parse().expect("bad number y")
         )
     }).collect();
-    println!("{:?}", tiles);
+    
+    let mut areas = vec![];
+    for p1 in tiles.iter() {
+        for p2 in tiles.iter() {
+            if p1 == p2 {
+                continue;
+            }
+            let area = (1 + (p1.0 - p2.0).abs()) * (1 + (p1.1 - p2.1).abs());
+            areas.push(area);
+        }
+    }
+    areas.sort();
+    *areas.iter().rev().take(1).next().expect("No answer")
 }
 
 fn p2(_raw_data: &str) {
