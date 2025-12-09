c205958b2e6dc38b4c81ceb1898886c68bf1f438
peetseater <peetseater@gmail.com>
2025-12-09 03:20:24 -0500

    D9P2 - progress - answer too high
    polygon point crossing seems like a better idea than using all the
    memory on my comptuer to make a giant hash set and yet

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -62,46 +62,46 @@ fn p2(raw_data: &str) -> ResultType {
     let mut min_y = ResultType::MAX;
     let mut max_y = ResultType::MIN;
 
-    println!("{:?}", "going green...");
-    let mut green_tiles = HashSet::new();
-    let n = red_tiles.len();
-    // Time to draw straight lines of green between red tiles.
-    for i in 0..n {
-        let first = red_tiles[i];
+    // println!("{:?}", "going green...");
+    // let mut green_tiles = HashSet::new();
+    // let n = red_tiles.len();
+    // // Time to draw straight lines of green between red tiles.
+    // for i in 0..n {
+    //     let first = red_tiles[i];
 
-        let second = if i + 1 != n {
-            red_tiles[i + 1]
-        } else {
-            red_tiles[0]
-        };
+    //     let second = if i + 1 != n {
+    //         red_tiles[i + 1]
+    //     } else {
+    //         red_tiles[0]
+    //     };
 
-        // there is a green STRAIGHT line between these two points.
-        let bx = first.0.min(second.0);
-        let bxMax = first.0.max(second.0);
-        let by = first.1.min(second.1);
-        let byMax = first.1.max(second.1);
-        for x in bx..=bxMax {
-            for y in by..=byMax {
-                // we technically count the red tiles as green, but that doesn't matter.
-                // its basically just a list of valid places to be
-                green_tiles.insert((x, y));
-            }
-        }
+    //     // there is a green STRAIGHT line between these two points.
+    //     let bx = first.0.min(second.0);
+    //     let bxMax = first.0.max(second.0);
+    //     let by = first.1.min(second.1);
+    //     let byMax = first.1.max(second.1);
+    //     for x in bx..=bxMax {
+    //         for y in by..=byMax {
+    //             // we technically count the red tiles as green, but that doesn't matter.
+    //             // its basically just a list of valid places to be
+    //             green_tiles.insert((x, y));
+    //         }
+    //     }
 
-        // running precompute.
-        if min_x > bx {
-            min_x = bx;
-        }
-        if max_x < bxMax {
-            max_x = bxMax;
-        }
-        if min_y > by {
-            min_y = by;
-        }
-        if max_y < byMax {
-            max_y = byMax;
-        }
-    }
+    //     // running precompute.
+    //     if min_x > bx {
+    //         min_x = bx;
+    //     }
+    //     if max_x < bxMax {
+    //         max_x = bxMax;
+    //     }
+    //     if min_y > by {
+    //         min_y = by;
+    //     }
+    //     if max_y < byMax {
+    //         max_y = byMax;
+    //     }
+    // }
     // But we're not done yet. We have completed the shape, and thus
     // we need to now fill the shape inside of it. This is the actual
     // hard part I suppose.
@@ -120,32 +120,32 @@ fn p2(raw_data: &str) -> ResultType {
     //     }
     // }
     // just choose one based roughtly on the loop connection and guesswork
-    let start_point = (97713,51514);
-    println!("Seeding at {:?}", start_point);
-    let mut queue = Vec::new();
-    queue.push(start_point);
-    while let Some(tile) = queue.pop() {
-        for dx in -1..=1 {
-            for dy in -1..=1 {
-                let new_tile = (tile.0 + dx, tile.1 + dy);
-                if new_tile.0 < min_x || new_tile.0 > max_x {
-                    println!("bad seed");
-                    continue;
-                }
-                if new_tile.1 < min_y || new_tile.1 > max_y {
-                    println!("bad seed");
-                    continue;
-                }
-                if green_tiles.contains(&new_tile) {
-                    continue;
-                } else {
-                    green_tiles.insert(new_tile.clone());
-                    queue.push(new_tile.clone());
-                    // println!("{:?}", new_tile);
-                }
-            }
-        }
-    }
+    // let start_point = (97713,51514);
+    // println!("Seeding at {:?}", start_point);
+    // let mut queue = Vec::new();
+    // queue.push(start_point);
+    // while let Some(tile) = queue.pop() {
+    //     for dx in -1..=1 {
+    //         for dy in -1..=1 {
+    //             let new_tile = (tile.0 + dx, tile.1 + dy);
+    //             if new_tile.0 < min_x || new_tile.0 > max_x {
+    //                 println!("bad seed");
+    //                 continue;
+    //             }
+    //             if new_tile.1 < min_y || new_tile.1 > max_y {
+    //                 println!("bad seed");
+    //                 continue;
+    //             }
+    //             if green_tiles.contains(&new_tile) {
+    //                 continue;
+    //             } else {
+    //                 green_tiles.insert(new_tile.clone());
+    //                 queue.push(new_tile.clone());
+    //                 // println!("{:?}", new_tile);
+    //             }
+    //         }
+    //     }
+    // }
 
 
 
@@ -155,8 +155,28 @@ fn p2(raw_data: &str) -> ResultType {
             if p1 == p2 {
                 continue;
             }
-            let area = (1 + (p1.0 - p2.0).abs()) * (1 + (p1.1 - p2.1).abs());
-            areas.push((area, p1, p2));
+            let width = 1 + (p1.0 - p2.0).abs();
+            let height = 1 + (p1.1 - p2.1).abs();
+            let area = width * height;
+            let mut inside = 0; 
+
+            let bx = p1.0.min(p2.0);
+            let bxMax = p1.0.max(p2.0);
+            let by = p1.1.min(p2.1);
+            let byMax = p1.1.max(p2.1);
+            let mid_x = (bx + bxMax) / 2;
+            let mid_y = (by + byMax) / 2;
+
+            let valid = point_in_poly(mid_x, mid_y, &red_tiles)
+            && point_in_poly(bx, by, &red_tiles)
+            && point_in_poly(bx, byMax, &red_tiles)
+            && point_in_poly(bxMax, by, &red_tiles)
+            && point_in_poly(bxMax, byMax, &red_tiles);
+
+            
+            if valid {
+                areas.push((area, p1, p2));
+            }
         }
     }
     areas.sort_by_key(|(a, _, _)| *a);
@@ -164,29 +184,50 @@ fn p2(raw_data: &str) -> ResultType {
     // Walk backward from the largest area to the smallest
     let mut largest_area = 0;
     for (area, p1, p2) in areas.iter().rev() {
-        let mut is_green = true;
-        let bx = p1.0.min(p2.0);
-        let bxMax = p1.0.max(p2.0);
-        let by = p1.1.min(p2.1);
-        let byMax = p1.1.max(p2.1);
-        for x in bx..=bxMax {
-            for y in by..=byMax {
-                // we technically count the red tiles as green, but that doesn't matter.
-                if !green_tiles.contains(&(x, y)) {
-                    is_green = false;
-                    break;
-                }
-            }
-            if !is_green {
-                break;
-            }
-        }
+        // let mut is_green = true;
+        // let bx = p1.0.min(p2.0);
+        // let bxMax = p1.0.max(p2.0);
+        // let by = p1.1.min(p2.1);
+        // let byMax = p1.1.max(p2.1);
+        // for x in bx..=bxMax {
+        //     for y in by..=byMax {
+        //         // we technically count the red tiles as green, but that doesn't matter.
+        //         if !green_tiles.contains(&(x, y)) {
+        //             is_green = false;
+        //             break;
+        //         }
+        //     }
+        //     if !is_green {
+        //         break;
+        //     }
+        // }
         // println!("{:?} {:?} {:?} {:?} {:?}", is_green, area, largest_area, p1, p2);
-        if is_green && *area > largest_area {
+        if *area > largest_area {
             largest_area = *area;
         }
     }
 
+    // 4492472302 answer too high
     largest_area
+}
+
+fn point_in_poly(x: ResultType, y: ResultType, poly: &Vec<(ResultType, ResultType)>) -> bool {
+    let mut crossings = 0;
+    let n = poly.len();
+
+    for i in 0..n {
+        let (x1, y1) = poly[i];
+        let (x2, y2) = poly[(i+1) % n];
+
+        // Check if ray crosses the edge
+        let intersects = ((y1 > y) != (y2 > y)) &&
+                         (x < (x2 - x1) * (y - y1) / (y2 - y1) + x1);
+
+        if intersects {
+            crossings += 1;
+        }
+    }
+
+    crossings % 2 == 1
 }
 
