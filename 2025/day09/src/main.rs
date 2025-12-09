6ed7b5962d992ed7d66686bf67e685d126cf1d8a
peetseater <peetseater@gmail.com>
2025-12-08 23:58:36 -0500

    Setup p1 and p2 empty methods

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = fs::read_to_string("./input").expect("bad input data");
+    let raw_data = raw_data.as_str();
+    let i = Instant::now();
+    let result = p1(raw_data);
+    let time = i.elapsed();
+    println!("{:?}", result);
+    println!("Took: {:?}", time);
+
+    let i = Instant::now();
+    let result = p2(raw_data);
+    let time = i.elapsed();
+    println!("{:?}", result);
+    println!("Took: {:?}", time);
 }
+
+fn p1(_raw_data: &str) {
+}
+
+fn p2(_raw_data: &str) {
+}
+
