1d07c257d33595ec078a6a849aa5f7d7c39e589e
peetseater <peetseater@gmail.com>
2025-12-09 00:56:18 -0500

    D9P2 - progress - green tileset incorrect
    Not viewing the example solution as green for some reason. Need to
    understand what I don't follow about hte straight lines here.

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::time::Instant;
+use std::collections::HashSet;
 
 fn main() {
     let raw_data = fs::read_to_string("./input").expect("bad input data");
@@ -42,7 +43,7 @@ fn p1(raw_data: &str) -> ResultType {
     *areas.iter().rev().take(1).next().expect("No answer")
 }
 
-fn p2(raw_data: &str) {
+fn p2(raw_data: &str) -> ResultType {
     let red_tiles: Vec<(ResultType, ResultType)> = raw_data.lines().take_while(|line| !line.is_empty()).map(|line| {
         let mut xy = line.split(",");
         (
@@ -51,7 +52,7 @@ fn p2(raw_data: &str) {
         )
     }).collect();
 
-    let mut green_tiles = vec![];
+    let mut green_tiles = HashSet::new();
     let n = red_tiles.len();
     // don't forget the edges.
     for i in 0..n {
@@ -71,13 +72,51 @@ fn p2(raw_data: &str) {
         for x in bx..=bxMax {
             for y in by..=byMax {
                 // we technically count the red tiles as green, but that doesn't matter.
-                green_tiles.push((x, y));
+                // its basically just a list of valid places to be
+                green_tiles.insert((x, y));
             }
         }
     }
-
     println!("GREEN{:?}", green_tiles);
 
+    let mut areas = vec![];
+    for p1 in red_tiles.iter() {
+        for p2 in red_tiles.iter() {
+            if p1 == p2 {
+                continue;
+            }
+            let area = (1 + (p1.0 - p2.0).abs()) * (1 + (p1.1 - p2.1).abs());
+            areas.push((area, p1, p2));
+        }
+    }
+    areas.sort_by_key(|(a, _, _)| *a);
 
+    // Walk backward from the largest area to the smallest
+    let mut largest_area = 0;
+    for (area, p1, p2) in areas.iter().rev() {
+        let mut is_green = true;
+        let bx = p1.0.min(p2.0);
+        let bxMax = p1.0.max(p2.0);
+        let by = p1.1.min(p2.1);
+        let byMax = p1.1.max(p2.1);
+        for x in bx..=bxMax {
+            for y in by..=byMax {
+                // we technically count the red tiles as green, but that doesn't matter.
+                if !green_tiles.contains(&(x, y)) {
+                    is_green = false;
+                    break;
+                }
+            }
+            if !is_green {
+                break;
+            }
+        }
+        println!("{:?} {:?} {:?} {:?} {:?}", is_green, area, largest_area, p1, p2);
+        if is_green && *area > largest_area {
+            largest_area = *area;
+        }
+    }
+
+    largest_area
 }
 
