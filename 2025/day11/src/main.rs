a9440de6cd01d0da7aa3ec9c6bf138204be1502b
peetseater <peetseater@gmail.com>
2025-12-11 00:43:01 -0500

    D11P2 naive nonworking solution, bed time

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -62,6 +62,44 @@ fn p1(raw_data: &str) -> ResultType {
 }
 
 
-fn p2(_raw_data: &str) -> ResultType {
-    0
+fn p2(raw_data: &str) -> ResultType {
+    // Parse into adjacency list: "aaa" => vec!["svr", "hhh"]
+    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for line in raw_data.lines().filter(|l| !l.is_empty()) {
+        let (left, rest) = line.split_once(':').unwrap();
+        let rights: Vec<&str> = rest.split_whitespace().collect();
+        graph.insert(left, rights);
+    }
+
+    // We're just doing DFS
+    let mut stack: Vec<Vec<&str>> = Vec::new();
+    stack.push(vec!["svr"]);
+
+    let mut count: ResultType = 0;
+
+    while let Some(path) = stack.pop() {
+        let last = path[path.len() - 1];
+
+        // If we reached "out", count this path
+        if last == "out" {
+            if path.contains(&"dac") && path.contains(&"fft") {
+                count += 1;
+            }
+            continue;
+        }
+
+        // Otherwise explore neighbors
+        if let Some(next_nodes) = graph.get(last) {
+            for &next in next_nodes {
+                if !path.contains(&next) {
+                    let mut new_path = path.clone();
+                    new_path.push(next);
+                    stack.push(new_path);
+                }
+            }
+        }
+    }
+
+    count
 }
