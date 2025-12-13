3c498675269680d1a1e860fee449ffa38cbafc1c
peetseater <peetseater@gmail.com>
2025-12-13 16:00:02 -0500

    D12 setup functions for p1 p2

--- 2025/day12/src/main.rs
+++ 2025/day12/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+use std::time::Instant;
+use std::collections::{HashMap, VecDeque};
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = fs::read_to_string("./input").expect("bad input data");
+    let raw_data = raw_data.as_str();
+    let i = Instant::now();
+    let result = p1(raw_data);
+    let time = i.elapsed();
+    println!("{:?}", result);
+    println!("Took: {:?}", time);
+
+    let i = Instant::now();
+    let result = p2(raw_data);
+    let time = i.elapsed();
+    println!("{:?}", result);
+    println!("Took: {:?}", time);
+}
+
+
+type ResultType = i64;
+fn p1(_raw_data: &str) -> ResultType {
+    0
+}
+
+
+fn p2(_raw_data: &str) -> ResultType {
+    0
 }
